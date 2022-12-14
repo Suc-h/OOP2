a7f57295ab00fbd99643981fd8b2cd6a165f0b18
Suc-h <97514706+Suc-h@users.noreply.github.com>
2022-12-14 16:41:19 +0100

    Oprava bugu s nakládáním auta

--- NakladAuto/Form1.Designer.cs
+++ NakladAuto/Form1.Designer.cs
@@ -34,13 +34,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label3 = new System.Windows.Forms.Label();
-            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown4 = new System.Windows.Forms.NumericUpDown();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
@@ -48,11 +54,13 @@
             this.button1.BackColor = System.Drawing.Color.Transparent;
             this.button1.BackgroundImage = global::NakladAuto.Properties.Resources._9815;
             this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.button1.Location = new System.Drawing.Point(12, 68);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(451, 60);
+            this.button1.Size = new System.Drawing.Size(260, 60);
             this.button1.TabIndex = 0;
+            this.button1.Text = "Vytvoř instanci ";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -72,7 +80,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Location = new System.Drawing.Point(24, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(27, 15);
             this.label1.TabIndex = 2;
@@ -120,6 +128,76 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Hmotnost Nákladu";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label4.Location = new System.Drawing.Point(285, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label5.Location = new System.Drawing.Point(278, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(172, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Hmotnost Vyloženého Nákladu";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::NakladAuto.Properties.Resources._9815;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Location = new System.Drawing.Point(291, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(134, 60);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Nalož Náklad";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::NakladAuto.Properties.Resources._9815;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.Font = new System.Drawing.Font("Showcard Gothic", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Location = new System.Drawing.Point(440, 68);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(134, 60);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Vylož Náklad";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.numericUpDown3.Location = new System.Drawing.Point(291, 39);
+            this.numericUpDown3.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(134, 23);
+            this.numericUpDown3.TabIndex = 9;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
             // numericUpDown2
             // 
             this.numericUpDown2.BackColor = System.Drawing.Color.Black;
@@ -139,45 +217,48 @@
             0,
             0});
             // 
-            // label4
+            // numericUpDown4
             // 
-            this.label4.AutoSize = true;
-            this.label4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(285, 12);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(0, 15);
-            this.label4.TabIndex = 7;
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(291, 12);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(172, 15);
-            this.label5.TabIndex = 8;
-            this.label5.Text = "Hmotnost Vyloženého Nákladu";
-            // 
-            // numericUpDown3
-            // 
-            this.numericUpDown3.BackColor = System.Drawing.Color.Black;
-            this.numericUpDown3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.numericUpDown3.Location = new System.Drawing.Point(291, 39);
-            this.numericUpDown3.Maximum = new decimal(new int[] {
+            this.numericUpDown4.BackColor = System.Drawing.Color.Black;
+            this.numericUpDown4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.numericUpDown4.Location = new System.Drawing.Point(440, 39);
+            this.numericUpDown4.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
-            this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(172, 23);
-            this.numericUpDown3.TabIndex = 9;
-            this.numericUpDown3.Value = new decimal(new int[] {
+            this.numericUpDown4.Name = "numericUpDown4";
+            this.numericUpDown4.Size = new System.Drawing.Size(134, 23);
+            this.numericUpDown4.TabIndex = 12;
+            this.numericUpDown4.Value = new decimal(new int[] {
             1,
             0,
             0,
             0});
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label6.Location = new System.Drawing.Point(468, 12);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(93, 15);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Nosnost Návěsu";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.checkBox1.Location = new System.Drawing.Point(581, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(61, 19);
+            this.checkBox1.TabIndex = 14;
+            this.checkBox1.Text = "Tahač?";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -185,7 +266,12 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(468, 135);
+            this.ClientSize = new System.Drawing.Size(654, 143);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.numericUpDown3);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -203,9 +289,11 @@
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.Text = "Nákladní Auto";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -219,9 +307,14 @@
         private Label label2;
         private NumericUpDown numericUpDown1;
         private Label label3;
-        private NumericUpDown numericUpDown2;
         private Label label4;
         private Label label5;
+        private Button button2;
+        private Button button3;
         private NumericUpDown numericUpDown3;
+        private NumericUpDown numericUpDown2;
+        private NumericUpDown numericUpDown4;
+        private Label label6;
+        private CheckBox checkBox1;
     }
 }
--- NakladAuto/NakladniAuto.cs
+++ NakladAuto/NakladniAuto.cs
@@ -1,68 +1,64 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NakladAuto
+﻿namespace NakladAuto
 {
     internal class NakladniAuto
     {
-        private string spz;
-        private int nosnost;
-        private int hmotnostNakladu = 0;
-       
+        protected string spz;
+        protected int nosnost;
+        protected int hmotnostNakladu = 0;
+
         public int HmotnostNakladu
         {
             get
             {
                 return hmotnostNakladu;
-            }            
+            }
         }
-        public NakladniAuto(string spz,int nosnost)
+        public NakladniAuto(string spz, int nosnost)
         {
             this.nosnost = nosnost;
             this.spz = spz;
         }
-        public void NalozNaklad(int hmotnost)
+        public virtual void NalozNaklad(int hmotnost)
         {
-            if(hmotnost>nosnost)
+            MessageBox.Show("Nakládám....");
+            if (hmotnost > nosnost)
             {
 
-                hmotnostNakladu = hmotnost-(hmotnost-nosnost);
+                hmotnostNakladu = nosnost;
 
 
                 MessageBox.Show("Náklad nebyl naložen celý, protože hmotnost je větší než nosnost! Nenaložilo se proto: " + (hmotnost - nosnost + "T"));
-                
+
             }
             else
             {
-                
 
-                hmotnostNakladu = hmotnost;
-                
+
+                hmotnostNakladu = hmotnostNakladu + hmotnost;
+
             }
         }
-        public void VylozNaklad(int hmotnost)
+        public virtual void VylozNaklad(int hmotnost)
         {
-            if(hmotnost>hmotnostNakladu)
+            MessageBox.Show("Vykládám....");
+            if (hmotnost > hmotnostNakladu)
             {
-               
+
                 MessageBox.Show("Chybí náklad:" + (hmotnost - hmotnostNakladu) + "T");
                 hmotnostNakladu = 0;
             }
             else
             {
-               
+
                 hmotnostNakladu = hmotnostNakladu - hmotnost;
-                
+
             }
         }
 
         public override string? ToString()
         {
-                return "Nákladní auto " + spz + " má nosnost: " + nosnost + "T a má naloženo " + hmotnostNakladu + "T";
-           
+            return "Nákladní auto " + spz + " má nosnost: " + nosnost + "T a má naloženo " + hmotnostNakladu + "T";
+
         }
     }
 }
